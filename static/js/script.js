c1433f982d2a4facf80a15a2d004a34fbf42eb4c
Saif Kayyali <saifkayyali2011@gmail.com>
2026-03-28 05:52:05 +0300

    Recent Cities button only fills in input box now + centered display

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('form');
     const submit = document.getElementById('button');
-    const Input = document.getElementById('city');
+    const input = document.getElementById('city');
+    const recentCity = document.querySelectorAll('.recent-city-btn')
 
-    if (!form || !submit || !Input) return;
+    if (!form || !submit || !input) return;
 
     const text = submit.textContent;
 
+    input.focus();
+
     form.addEventListener('submit', (event) => {
-        const city = Input.value.trim();
+        const city = input.value.trim();
         const submitter = event.submitter;
 
         if (!submitter) return;
-
-        if (submitter.tagName === "BUTTON" && submitter.name === "recentCity") return;
 
         if (!city || !/^[\p{L}\s,.-]+$/u.test(city)) {
             event.preventDefault();
@@ -21,12 +22,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
             submit.disabled = false;
             submit.textContent = text;
-            Input.focus();
+            input.focus();
             return;
         }
 
         submit.classList.add('is-loading');
         submit.disabled = true;
+    });
+
+    recentCity.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const selected = btn.getAttribute('data-city');
+            input.value = selected;
+            input.focus();
+        });
     });
 
     const table = document.getElementById("table");
